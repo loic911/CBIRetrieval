198adde3b3c863841090f22bdc726ee60c48119e
Loïc Rollus <loicrollus@gmail.com>
2015-03-26 09:31:42 +0100

    README + package

--- package.sh
+++ package.sh
@@ -2,6 +2,7 @@
 NOW=$(date +"%Y%m%d")
 rm -f CBIRetrieval*.zip
 rm -f bin/*.jar
+mvn clean
 mvn package -Dmaven.test.skip=true
 cp target/Java*with-dependencies.jar bin/
 zip -r CBIRetrieval-$NOW.zip *
