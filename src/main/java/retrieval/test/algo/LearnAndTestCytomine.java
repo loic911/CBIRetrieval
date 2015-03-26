e0840d8af37351c29a4681fd8fa757d31d53605e
Loïc Rollus <loicrollus@gmail.com>
2015-03-26 09:45:45 +0100

    add license file

--- src/main/java/retrieval/test/algo/LearnAndTestCytomine.java
+++ src/main/java/retrieval/test/algo/LearnAndTestCytomine.java
@@ -18,9 +18,20 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.stream.Collectors;
-
-/**
- * Created by lrollus on 09/02/15.
+/*
+ * Copyright 2015 ROLLUS Loïc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class LearnAndTestCytomine {
 
